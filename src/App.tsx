68e75393b2a17a775aaa9140d1ecf16f62bff86f
Aayush Raut <aayussraut.ar@gmail.com>
2024-03-13 14:59:47 +0545

    feat: shadcn configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div className="text-red-500">Hello World</div>;
+  return (
+    <div className="text-red-500">
+      Hello World
+      <Button variant={"outline"}>Test</Button>
+    </div>
+  );
 }
 
 export default App;
