5bf74692d7d7a625b0136f7ac2fcfad4ea582edd
Aayush Raut <aayussraut.ar@gmail.com>
2024-03-14 14:22:50 +0545

    feat: react router config

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,36 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/index.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/home-page.tsx";
+import MoviePage from "./pages/movie-page.tsx";
+import MovieDetailPage from "./pages/movie-detail-page.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/movie",
+        element: <MoviePage />,
+      },
+      {
+        path: "/movie/:id",
+        element: <MovieDetailPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
