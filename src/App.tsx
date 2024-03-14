e206a8b2ef884f500269aaf84084b49d5622a0c0
Aayush Raut <aayussraut.ar@gmail.com>
2024-03-14 11:34:11 +0545

    add top rated movie slider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import NowShowingSlider from "./components/now-showing/now-showing-slider";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import TopRatedMovieSlider from "./components/top-rated-movie/top-rated-movie-slider";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div className="mx-auto w-[1280px]">
           <div>
             <NowShowingSlider />
+            <TopRatedMovieSlider />
           </div>
         </div>
       </main>
