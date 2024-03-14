59a7e25ef29a007138228750f54ab37f26036c75
Aayush Raut <aayussraut.ar@gmail.com>
2024-03-14 06:51:21 +0545

    feat: add now showing slider

---
+++ src/components/now-showing/now-showing-slider.tsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
+
+import NowShowingCard from "./now-showing-card";
+import { fetchNowShowing } from "@/action";
+import { MovieState } from "@/reducers";
+
+interface NowShowingSliderProps {
+  movies: MovieState;
+  fetchNowShowing: () => void;
+}
+
+const NowShowingSlider = ({
+  movies,
+  fetchNowShowing,
+}: NowShowingSliderProps) => {
+  useEffect(() => {
+    fetchNowShowing();
+  }, []);
+
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    // arrows: false,
+    // dots: true,
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        {movies.nowShowing.map((movie) => (
+          <NowShowingCard key={movie.id} movie={movie} />
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchNowShowing: () => dispatch(fetchNowShowing()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NowShowingSlider);
