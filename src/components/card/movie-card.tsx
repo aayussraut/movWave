f8c7a3a064ebc9135183d9b8d89d8f5e4b963d33
Aayush Raut <aayussraut.ar@gmail.com>
2024-03-14 11:33:56 +0545

    add a movie card

---
+++ src/components/card/movie-card.tsx
@@ -0,0 +1,49 @@
+import { MdStarRate, MdOutlineRemoveRedEye } from "react-icons/md";
+
+import { Movie } from "../../reducers/index";
+import { Button } from "../ui/button";
+
+interface MovieCardProps {
+  movie: Movie;
+}
+
+const MovieCard = ({ movie }: MovieCardProps) => {
+  return (
+    <div className="flex flex-col bg-[#060B1C] px-4 py-2 gap-2 h-auto w-56 mx-10 my-2 first:ms-4 last:me-0 rounded-lg shadow-lg">
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        alt={movie.title}
+        className="w-52 h-60 object-scale-down rounded-md mx-auto"
+      />
+
+      <div className="flex justify-between">
+        <h1 className="text-white text-md font-bold h-12 line-clamp-2">
+          {movie.title}
+        </h1>
+        <div className="flex bg-yellow-200/15 px-1 rounded-xl gap-2 items-center justify-center h-fit py-1 ">
+          <MdStarRate fill={"yellow"} size={14} />
+          <p className="text-yellow-200 text-xs font-semibold">
+            {movie.vote_average.toFixed(1)}
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <p className="text-xs">Release: {movie.release_date}</p>
+        <p className="text-xs">Genre: Action, Adventure</p>
+      </div>
+
+      <div>
+        <Button
+          variant="default"
+          className="flex items-center gap-2 bg-purple-900 w-full"
+        >
+          <MdOutlineRemoveRedEye size={18} fill="white" />
+          <span className="text-white">View Details</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
