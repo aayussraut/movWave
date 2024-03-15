105b4cc68b0af633759359ca9fb1d6621a137c3f
Aayush Raut <aayussraut.ar@gmail.com>
2024-03-15 10:40:50 +0545

    feat: add search, descover and similar movie option in reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -18,7 +18,10 @@ export interface MovieState {
   nowShowing: Movie[];
   upcoming: Movie[];
   topRated: Movie[];
+  discoveredMovies: Movie[];
+  searchResults: Movie[];
   selectedMovie: Movie | null;
+  similarMovies: Movie[];
 }
 
 interface Action {
@@ -30,7 +33,10 @@ const initialState: MovieState = {
   nowShowing: [],
   upcoming: [],
   topRated: [],
+  discoveredMovies: [],
+  searchResults: [],
   selectedMovie: null,
+  similarMovies: [],
 };
 
 const movieReducer = (state = initialState, action: Action) => {
@@ -41,8 +47,14 @@ const movieReducer = (state = initialState, action: Action) => {
       return { ...state, upcoming: action.payload };
     case "FETCH_TOP_RATED":
       return { ...state, topRated: action.payload };
+    case "SEARCH_MOVIES":
+      return { ...state, searchResults: action.payload };
     case "FETCH_MOVIE_DETAILS":
       return { ...state, selectedMovie: action.payload };
+    case "DISCOVER_MOVIES":
+      return { ...state, discoveredMovies: action.payload };
+    case "SIMILAR_MOVIES":
+      return { ...state, similarMovies: action.payload };
     default:
       return state;
   }
